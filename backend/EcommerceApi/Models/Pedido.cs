486c0a94013af049aefbba066bf2b4b260813f31
Cylliun <japaguilherme55@gmail.com>
2025-04-28 01:39:19 -0400

    Header do sistema finalizado + cores do background

--- backend/EcommerceApi/Models/Pedido.cs
+++ backend/EcommerceApi/Models/Pedido.cs
@@ -3,11 +3,8 @@
     public class Pedido
     {
         public int Id { get; set; }
-
         public DateTime DataCriacao { get; set; } = DateTime.Now;
-
         public List<ItemPedido> Itens { get; set; } = new List<ItemPedido>();
-
         public decimal Total => Itens.Sum(item => item.Quantidade * item.PrecoUnitario);
     }
 }
--- frontend/ecommerce-app/src/app/app.routes.ts
+++ frontend/ecommerce-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { CartComponent } from './core/features/cart/cart.component';
+import { CheckoutComponent } from './core/features/checkout/checkout.component';
+import { ProductsComponent } from './core/features/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'produtos', component: ProductsComponent},
+    {path: 'carrinho', component: CartComponent},
+    {path: 'checkout', component: CheckoutComponent},
+    {path: '', redirectTo: 'produtos', pathMatch: 'full'}
+];
+
